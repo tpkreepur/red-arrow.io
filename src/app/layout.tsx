a070b793bceede54b4df1b44ac82375ed94b5b90
Justin Moore <ajustinmoore86@gmail.com>
2026-03-30 15:31:18 -0700

    feat: add Google Tag Manager integration and update package dependencies

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { type Metadata } from "next";
 import { Providers } from "@/app/providers";
 import { Layout } from "@/components/Layout";
 import { getSiteUrl } from "@/lib/site";
+import { GoogleTagManager } from "@next/third-parties/google";
 
 import "@/styles/tailwind.css";
 
@@ -16,11 +17,7 @@ export const metadata: Metadata = {
   },
   description:
     "I’m Justin, a software designer and entrepreneur based in Southern California. I’m the founder and CEO of Red Arrow Solutions, where we develop technologies that empower regular people and small businesses on their own terms.",
-  alternates: {
-    types: {
-      "application/rss+xml": `${siteUrl}/feed.xml`,
-    },
-  },
+  alternates: { types: { "application/rss+xml": `${siteUrl}/feed.xml` } },
 };
 
 export default function RootLayout({
@@ -30,6 +27,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="h-full antialiased" suppressHydrationWarning>
+      <GoogleTagManager gtmId="G-R4NPWFQ1ZG" />
       <body className="flex h-full bg-zinc-50 dark:bg-black">
         <Providers>
           <div className="flex w-full">
